5cfbe71afb69689f9f7db1042fc987a5a2408617
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-07-18 21:40:38 +0100

    - creTING NAVBAR

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,9 +1,27 @@
 "use client";
+import { PRODUCT_CATEGORIES } from "@/lib/data";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
-	return <div className="flex gap-4 h-full">NavItems</div>;
+   return (
+			<div className="flex gap-4 h-full">
+				{PRODUCT_CATEGORIES.map((category, i) => {
+					const handleOpen = () => {
+						if (activeIndex === i) {
+							setActiveIndex(null);
+						} else {
+							setActiveIndex(i);
+						}
+					};
+
+					const isOpen = i === activeIndex;
+
+					return <NavItem />;
+				})}
+			</div>
+		);
 };
 
 export default NavItems;
