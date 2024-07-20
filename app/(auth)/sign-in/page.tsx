ee167ddf64bedb317a62e0191e522516cf4dd7e3
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-07-20 22:57:22 +0100

    - successfully completed signup and sign in flow

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -17,9 +17,24 @@ import {
 } from "@/lib/validators/account-credentials-validator";
 import { trpc } from "@/lib/trpc/client";
 import { ZodError } from "zod";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const page = () => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+
+	const isSeller = searchParams.get("as") === "seller";
+
+	const origin = searchParams.get("origin");
+
+	const continueAsSeller = () => {
+		router.push("?as=seller");
+	};
+
+	const continueAsBuyer = () => {
+		router.replace("/sign-in", undefined);
+	};
+
 	const {
 		register,
 		handleSubmit,
@@ -28,34 +43,33 @@ const page = () => {
 		resolver: zodResolver(AuthCredentialValidator),
 	});
 
-   const router = useRouter();
+	const { mutate: signIn, isLoading } = trpc.auth.signIn.useMutation({
+		onSuccess: () => {
+			toast.success("Signed in successfully");
+			router.refresh();
 
-		const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
-			onError: (err) => {
-				if (err.data?.code === "CONFLICT") {
-					toast.error("This email is already in use, Sign in instead?");
+			if (origin) {
+				router.push(`/${origin}`);
+			}
 
-					return;
-				}
+			if (isSeller) {
+				router.push("/sell");
+				return;
+			}
 
-				if (err instanceof ZodError) {
-					toast.error(err.issues[0].message);
+			router.push("/");
+		},
 
-					return;
-				}
-
-				toast.error("Something went wrong, Please try again.");
-			},
-
-			onSuccess: ({ sentToEmail }) => {
-				toast.success(`Verication email sent to ${sentToEmail}`);
-				router.push("/verify-email?to=" + sentToEmail);
-			},
-		});
+		onError: (err) => {
+			if (err.data?.code === "UNAUTHORIZED") {
+				toast.error("Invalid email or password");
+			}
+		},
+	});
 
 	const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
 		// SEND DATA TO SERVER
-		mutate({ email, password });
+		signIn({ email, password });
 	};
 	return (
 		<>
@@ -63,16 +77,18 @@ const page = () => {
 				<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 					<div className="flex flex-col items-center space-y-2 text-center">
 						<Icons.logo className="h-20 w-20" />
-						<h1 className="text-2xl font-bold">Create an Account</h1>
+						<h1 className="text-2xl font-bold">
+							Sign in to your {isSeller ? "seller" : ""} account
+						</h1>
 
 						<Link
-							href="sign-in"
+							href="sign-up"
 							className={buttonVariants({
 								variant: "link",
 								className: "gap-1.5",
 							})}
 						>
-							Already got an account? Sign in
+							Don&apos;t have an account? Sign up
 							<ArrowRight className="h-4 w-4" />
 						</Link>
 					</div>
@@ -113,9 +129,42 @@ const page = () => {
 									)}
 								</div>
 
-								<Button type="submit">Sign up</Button>
+								<Button type="submit">Sign in</Button>
 							</div>
 						</form>
+
+						<div className="relative">
+							<div
+								aria-hidden="true"
+								className="absolute inset-0 flex items-center"
+							>
+								<span className="w-full border-t" />
+							</div>
+
+							<div className="relative flex justify-center text-xs uppercase">
+								<span className="bg-background px-2 text-muted-foreground">
+									or
+								</span>
+							</div>
+						</div>
+
+						{isSeller ? (
+							<Button
+								onClick={continueAsBuyer}
+								variant={"secondary"}
+								disabled={isLoading}
+							>
+								Continue as customer?
+							</Button>
+						) : (
+							<Button
+								onClick={continueAsSeller}
+								variant={"secondary"}
+								disabled={isLoading}
+							>
+								Continue as seller
+							</Button>
+						)}
 					</div>
 				</div>
 			</div>
