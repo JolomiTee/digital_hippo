cbeacdf9b754aa8a171c78ddc43fcb3d7b1832e1
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-07-26 23:37:53 +0100

    - added swiper carousel library - displayed items on the frontend

--- components/ProductReel.tsx
+++ components/ProductReel.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { trpc } from "@/lib/trpc/client";
-import { TQueryVAlidator } from "@/lib/validators/QueryValidator";
 import Link from "next/link";
+import { TQueryVAlidator } from "@/lib/validators/QueryValidator";
+import { trpc } from "@/lib/trpc/client";
+import { Product } from "@/app/payload-types";
+import ProductListing from "./ProductListing";
 
 interface ProductReelProps {
 	title: string;
@@ -16,30 +18,37 @@ const FALLBACK_LIMIT = 4;
 const ProductReel = (props: ProductReelProps) => {
 	const { title, subtitle, href, query } = props;
 
-	const { data } = trpc.getInfinitProducts.useInfiniteQuery(
-		{
-			limit: query.limit ?? FALLBACK_LIMIT,
-			query,
-		},
-		{
-			getNextPageParam: (lastPage) => lastPage.nextPage,
-		}
-	);
+	const { data: queryResults, isLoading } =
+		trpc.getInfinitProducts.useInfiniteQuery(
+			{
+				limit: query.limit ?? FALLBACK_LIMIT,
+				query,
+			},
+			{
+				getNextPageParam: (lastPage) => lastPage.nextPage,
+			}
+		);
 
-   console.log(data);
+	const products = queryResults?.pages.flatMap((page) => page.items);
+
+	var map: (Product | null)[] = [];
+	if (products && products.length) {
+		map = products;
+	} else if (isLoading) {
+		map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null);
+	}
 
 	return (
 		<section className="py-12">
-			<div className="md:flex md:items-center md:justify-between">
+			<div className="md:flex md:items-center md:justify-between mb-4">
 				<div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
 					{title ? (
 						<h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
 							{title}
 						</h1>
 					) : null}
-
 					{subtitle ? (
-						<p className="mt-2 text-sm text-muted-foreground">Brand New</p>
+						<p className="mt-2 text-sm text-muted-foreground">{subtitle}</p>
 					) : null}
 				</div>
 
@@ -55,7 +64,15 @@ const ProductReel = (props: ProductReelProps) => {
 
 			<div className="relative">
 				<div className="mt-6 flex items-center w-full">
-					<div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8"></div>
+					<div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
+						{map.map((product, i) => (
+							<ProductListing
+								key={`product-${i}`}
+								product={product}
+								index={i}
+							/>
+						))}
+					</div>
 				</div>
 			</div>
 		</section>
